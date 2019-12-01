7eb5a13651c478e9ebd4566696c87919557b49b8
sahara2001 <sun201709@outlook.com>
2019-12-01 15:26:23 -0500

    hog hand

--- hog.py
+++ hog.py
@@ -1,1 +1,45 @@
 import features 
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+im1 = cv2.imread('output/000045.png')
+
+template = cv2.imread('output/template.png')
+
+template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
+im1 = im1[im1.shape[0]-720:,im1.shape[1]-720:,:]
+template = template[50:350,50:350,:]
+print(template.shape,im1.shape)
+tmp = features.hog(template)
+
+img = features.hog(im1)
+
+print(img.shape,tmp.shape)
+r,c,_ = tmp.shape
+sse = np.zeros(img.shape[:2])
+# print
+for i in range(img.shape[0]-r):
+    for j in range(img.shape[1]-c):
+        sse[i,j] = np.sqrt(np.square(img[i:i+r,j:j+c,:] - tmp).sum(axis=(0,1))).sum()
+
+tgt = np.argmin(sse[:img.shape[0]-r,:img.shape[1]-c])
+match = np.unravel_index(tgt, (img.shape[0]-r,img.shape[1]-c))
+print(match)
+a = plt.figure()
+ax = a.add_subplot(121)
+plt.imshow(sse[:img.shape[0]-r,:img.shape[1]-c],cmap='gray')
+ax = a.add_subplot(122)
+plt.imshow(im1)
+plt.show()
+# print('best match position:',match)
+# print('best candidate distance:',scores.min())
+# a = plt.figure()
+# ax = a.add_subplot(131)
+# plt.imshow(im1)
+# rect = plt.Rectangle(( match[1],match[0]),c,r, color='r', alpha=0.4)
+# ax.add_patch(rect)
+
+# a.add_subplot(132)
+# plt.imshow(im1[match[0]:match[0]+r, match[1]:match[1]+c])
+# plt.title('Problem 1 Result')
--- modules/hog.py
+++ modules/hog.py
@@ -1,8 +1,13 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
+im1 = cv2.imread('../output/000045.png')
 
+template = cv2.imread('../output/template.png')
+
+template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
 
 winSize = (64,64)
 blockSize = (16,16)
@@ -15,5 +20,58 @@ histogramNormType = 0
 L2HysThreshold = 2.0000000000000001e-01
 gammaCorrection = 0
 nlevels = 64
+
 hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
                         histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
+
+img = hog.compute(im1)
+tmp = hog.compute(im1[::2,::2,:])
+print(img.shape,tmp.shape)
+
+winStride = (4,4)
+padding = (8,8)
+locations = ((20,20),)
+
+#hist = hog.compute(im1[::2,::2,:],winStride,padding,locations)
+
+
+meanShift = True
+patch = (350,850)
+# padding = (100,100)
+template = im1[290:450,750:950,:]
+print(template.shape)
+locations = (patch,)
+hist = hog.compute(im1,winStride,padding,locations)
+print(1)
+# print(cv2.HOGDescriptor_getDefaultPeopleDetector().shape)
+hog.setSVMDetector(hist)
+r,c,_ = template.shape
+#im1 = im1[800:1000,250:350,:]
+sse = np.zeros(im1.shape[:2])
+print(1.5)
+start = datetime.datetime.now()
+(rects, weights) = hog.detectMultiScale(im1, winStride=winStride,padding=padding,scale=1.5, useMeanshiftGrouping=meanShift)
+print(2)
+print(rects)
+for ( x,y,w,h) in rects:
+    cv2.rectangle(im1,(x,y), (x+w, y+h), (0,255,0), 2)
+
+
+# print
+# print(im1.shape)
+# w= 800
+# h = 250
+# for i in range(0,200,10):
+#     for j in range(0,200,10):
+#         locations = ((h+i,w+j),)
+#         desc = hog.compute(im1,winStride,padding,locations)
+#         sse[i,j] = np.sqrt(np.square(desc - hist).sum(axis=(0,1))).sum()
+
+# print(np.unravel_index(np.argmin(sse),template.shape[:2]))
+print((datetime.datetime.now()-start).total_seconds())
+a = plt.figure()
+a.add_subplot(121)
+plt.imshow(im1)
+a.add_subplot(122)
+plt.imshow(template,cmap='gray')
+plt.show()
